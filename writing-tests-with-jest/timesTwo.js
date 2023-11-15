a62c93e8e6145db70d73f22c6d2872bacbf5be5e
MirageM <imiragesz@gmail.com>
2023-11-15 00:19:18 -0500

    module.exports = timesTwo;

---
+++ writing-tests-with-jest/timesTwo.js
@@ -0,0 +1,10 @@
+// Task 1: Code the timesTwo function declaration
+// Open the timesTwo.js file and add a function named `timesTwo`. The function should take number as input and return the value 2 multiplied by the number.
+// Export the timesTwo function as a module. 
+function timesTwo(number) {
+    return 2 * number;
+}
+
+// Task 2: Export the timesTwo function as a module
+
+module.exports = timesTwo;
---
+++ writing-tests-with-jest/timesTwo.test.js
@@ -0,0 +1,9 @@
+const timesTwo = require('./timesTwo');
+
+// Write the first test
+// Code a test call with the following arguments: 
+// 1. The description that reads: "returns the number times 2". 
+// 2. The second argument should expect the call to the timesTwo function, when passed the number 10, to be 20.
+test('returns the number times 2', () => {
+    expect(timesTwo(10)).toBe(20);
+});
